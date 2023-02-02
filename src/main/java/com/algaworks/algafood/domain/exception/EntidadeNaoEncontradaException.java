f8eff4792c6249dbc220f6ada0216e38677754e1
sadrack404 <gabrielscr2023@gmail.com>
2023-02-02 12:40:08 -0300

    modo de se usar o ResponseStatusExection

--- src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,11 +1,18 @@
 package com.algaworks.algafood.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)//, reason = "Entidade não encontrada") <- mostra uma msg no erro
-public class EntidadeNaoEncontradaException extends RuntimeException {
-    public EntidadeNaoEncontradaException(String message) {
-        super(message);
+//@ResponseStatus(value = HttpStatus.NOT_FOUND)//, reason = "Entidade não encontrada") <- mostra uma msg no erro
+public class EntidadeNaoEncontradaException extends ResponseStatusException {
+//Construtor flexivel onde podemos passar um status e uma mensagem
+//é um modo menos pior de se usar o responseStatusException
+    public EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
+        super(status, mensagem);
+    }
+
+    //Construtor padrão, onde o THIS vai invocar uma resposta HTTP do construtor a cima
+    public EntidadeNaoEncontradaException(String mensagem) {
+        this(HttpStatus.NOT_FOUND, mensagem);
     }
 }
