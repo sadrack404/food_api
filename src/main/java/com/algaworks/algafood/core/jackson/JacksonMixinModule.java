140caa2c2463edbe6d6d3342b7c003385d0b0ed6
sadrack404 <gabrielscr2023@gmail.com>
2023-02-21 19:14:53 -0300

    entendido o conceito de serializacao e mixins

---
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -0,0 +1,11 @@
+package com.algaworks.algafood.core.jackson;
+
+import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+public class JacksonMixinModule extends SimpleModule {
+    public JacksonMixinModule() {
+    setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+    }
+}
--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -4,7 +4,6 @@ import com.algaworks.algafood.core.validation.Groups;
 import com.algaworks.algafood.core.validation.Multiplo;
 import com.algaworks.algafood.core.validation.TaxaFrete;
 import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +14,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import javax.validation.Valid;
-import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.groups.ConvertGroup;
@@ -59,27 +57,22 @@ public class Restaurante {
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
-    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
             joinColumns = @JoinColumn(name = "restaurante_id"),
             inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
     private List<FormaPagamento> formasDePagamento = new ArrayList<>();
 
-    @JsonIgnore
     @Embedded
     private Endereco endereco;
 
-    @JsonIgnore
     @CreationTimestamp //Vai Instanciar uma data no momento em que for criada
     @Column(nullable = false, columnDefinition = "datatime")
     private LocalDate dataCadastro;
 
-    @JsonIgnore
     @UpdateTimestamp//Vai Atualizar uma data no momento em que for criada
     @Column(nullable = false, columnDefinition = "datatime")
     private LocalDate dataAtualizacao;
-    @JsonIgnore
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produto = new ArrayList<>();
 
