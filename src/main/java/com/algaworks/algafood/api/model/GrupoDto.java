eb382706195dcced5c8d1825c1936881343af6b2
sadrack404 <gabrielscr2023@gmail.com>
2023-03-09 19:39:56 -0300

    crud de grupo finalizado

---
+++ src/main/java/com/algaworks/algafood/api/model/GrupoDto.java
@@ -0,0 +1,12 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GrupoDto {
+
+    private Long id;
+    private String nome;
+}
